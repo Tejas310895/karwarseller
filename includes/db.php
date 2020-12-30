4713ec5236589f053e7d1f562feedb3efc7a6240
tshirsat700@gmail.com <tshirsat700@gmail.com>
2020-12-30 23:33:09 +0530

    first commit

---
+++ includes/db.php
@@ -0,0 +1,10 @@
+<?php 
+
+$con = mysqli_connect('localhost','u464233779_karwars','Akshay@11','u464233779_karwars');
+
+/*if($con){
+    echo " connected ";
+}else{
+    echo " not connected :" . mysqli_connect_error();
+}*/
+?>
