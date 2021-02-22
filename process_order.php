d17c1fb9405dda89825b9f1c011cf20e0a8ed76b
tshirsat700@gmail.com <tshirsat700@gmail.com>
2021-02-22 18:41:12 +0530

    error

--- process_order.php
+++ process_order.php
@@ -1,6 +1,18 @@
 <?php 
-
+session_start();
 include("includes/db.php");
+
+if(!isset($_SESSION['client_email'])){
+
+  echo "<script>window.open('login.php','_self')</script>";
+
+}else{
+
+  $seller_email = $_SESSION['client_email'];
+  $get_client_id = "select * from clients where client_email='$seller_email'";
+  $run_client_id = mysqli_query($con,$get_client_id);
+  $row_client_id = mysqli_fetch_array($run_client_id);
+  $client_id = $row_client_id['client_id'];
 
 if(isset($_GET['update_order'])){
 
@@ -11,7 +23,7 @@ if(isset($_GET['update_order'])){
 
   $status = $_GET['status'];
 
-  $update_status_del = "UPDATE customer_orders SET order_status='$status',del_date='$today' WHERE invoice_no='$update_order'";
+  $update_status_del = "UPDATE customer_orders SET order_status='$status',del_date='$today' WHERE invoice_no='$update_order' and client_id='$client_id'";
 
   $run_status_del = mysqli_query($con,$update_status_del);
 
@@ -20,6 +32,8 @@ if(isset($_GET['update_order'])){
 
     echo "<script>window.open('index.php?orders','_self')</script>";
 
+
+}
 
 }
 
